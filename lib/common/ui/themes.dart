7a9835c801590f8c459f3e56c7407f98014f4819
Дмитрий Заречнев <zarechnevd@gmail.com>
2021-08-25 11:16:36 +0300

    Modified Themes class

--- lib/common/ui/themes.dart
+++ lib/common/ui/themes.dart
@@ -4,5 +4,5 @@ import 'package:template_bloc/common/ui/app_colors.dart';
 final ThemeData lightTheme = ThemeData(
   fontFamily: 'Roboto',
   visualDensity: VisualDensity.adaptivePlatformDensity,
-  primaryColor: AppColors.primaryColor,
+  accentColor: AppColors.accent,
 );
