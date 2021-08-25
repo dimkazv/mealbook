d08b081d8acdd90aee6059d6766a8baa5abef74c
Дмитрий Заречнев <zarechnevd@gmail.com>
2021-08-25 11:14:52 +0300

    Setuped AppColors and AppAssets

---
+++ lib/common/ui/app_assets.dart
@@ -0,0 +1,7 @@
+class AppAssets {
+  static const splashPic = 'res/assets/splash.jpg';
+
+  static const homeIcon = 'res/assets/home.svg';
+  static const favoriteIcon = 'res/assets/favorite.svg';
+  static const shoppingListIcon = 'res/assets/shopping_list.svg';
+}
--- lib/common/ui/app_colors.dart
+++ lib/common/ui/app_colors.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const primaryColor = Colors.black;
-  static const textPrimaryColor = Colors.black;
+  static const background = Color(0xFFFAFCFD);
+  static const primary = Color(0xFFFFFFFF);
+  static const accent = Color(0xFF3ABD69);
+  static const textFaded = Color(0xFFBDBDBD);
+  static const white = Color(0xFFFFFFFF);
+  static const textPrimary = Colors.black;
+  static const bottomBarIconSelected = Colors.black;
+  static const bottomBarIcon = Color(0xFFDADADA);
+  static const bottomBarShadow = Colors.black;
 }
-
-
