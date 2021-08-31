be7ea429f22f4c79d06608ef2d06890de572e84d
Дмитрий Заречнев <zarechnevd@gmail.com>
2021-08-31 13:56:54 +0300

    Setup AppColors

--- lib/common/ui/app_colors.dart
+++ lib/common/ui/app_colors.dart
@@ -4,10 +4,17 @@ class AppColors {
   static const background = Color(0xFFFAFCFD);
   static const primary = Color(0xFFFFFFFF);
   static const accent = Color(0xFF3ABD69);
+  static const ingredientCard = Color(0xFFF6F8FC);
   static const textFaded = Color(0xFFBDBDBD);
   static const white = Color(0xFFFFFFFF);
-  static const textPrimary = Colors.black;
-  static const bottomBarIconSelected = Colors.black;
+  static const action = Color(0xFF5E5E5E);
+  static const textPrimary = Color(0xFF000000);
+  static const bottomBarIconSelected = Color(0xFF000000);
   static const bottomBarIcon = Color(0xFFDADADA);
-  static const bottomBarShadow = Colors.black;
+  static const bottomBarShadow = Color(0xFF000000);
+  static const shimmerBase = Color(0xFFECEFF1);
+  static const shimmerHighlight = Color(0xFFFFFFFF);
+  static const shimmerContainer = Color(0xFFFFFFFF);
+  static const imageGradientTransparent = Color(0x00000000);
+  static const imageGradientDark = Color(0xB3000000);
 }
