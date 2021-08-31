217dc5b410d8f7e241ef7614f3082ce386666d3c
Дмитрий Заречнев <zarechnevd@gmail.com>
2021-08-31 13:45:51 +0300

    Add recipe card widget

--- lib/common/ui/widgets/recipe_card.dart
+++ lib/common/ui/widgets/recipe_card.dart
@@ -1,88 +1,76 @@
 import 'dart:ui';
 
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:mealbook/common/ui/app_assets.dart';
 import 'package:mealbook/common/ui/app_colors.dart';
 
 class RecipeCard extends StatelessWidget {
-  const RecipeCard({Key? key}) : super(key: key);
+  const RecipeCard({
+    required Function() onTap,
+    required String title,
+    required String image,
+    Key? key,
+  })  : _onTap = onTap,
+        _title = title,
+        _image = image,
+        super(key: key);
+
+  final Function() _onTap;
+  final String _title;
+  final String _image;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 400,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(32),
-        image: const DecorationImage(
-          image: AssetImage(AppAssets.splashPic),
-          fit: BoxFit.cover,
-        ),
-      ),
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(32),
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [Colors.transparent, Colors.black.withOpacity(0.7)],
-          ),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Align(
-                alignment: Alignment.topRight,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(24),
-                  child: SizedBox(
-                    width: 78,
-                    height: 78,
-                    child: BackdropFilter(
-                      filter: ImageFilter.blur(sigmaX: 20, sigmaY: 20),
-                      child: Material(
-                        color: Colors.transparent,
-                        child: InkWell(
-                          onTap: () {},
-                          child: const Icon(
-                            Icons.favorite_outline,
-                            color: AppColors.primary,
-                            size: 40,
-                          ),
+    return Stack(
+      children: [
+        Material(
+          color: Colors.transparent,
+          child: InkWell(
+            borderRadius: BorderRadius.circular(32),
+            onTap: _onTap,
+            child: Container(
+              height: 400,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(32),
+                image: DecorationImage(
+                  image: CachedNetworkImageProvider(_image),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(32),
+                  gradient: const LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      AppColors.imageGradientTransparent,
+                      AppColors.imageGradientDark,
+                    ],
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(20, 0, 20, 40),
+                      child: Text(
+                        _title,
+                        style: const TextStyle(
+                          color: AppColors.primary,
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
                     ),
-                  ),
+                  ],
                 ),
               ),
             ),
-            const Spacer(),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: Text(
-                'Salmon Sushi Matcha',
-                style: TextStyle(
-                  color: AppColors.primary,
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            const Padding(
-              padding: EdgeInsets.fromLTRB(20, 20, 24, 30),
-              child: Text(
-                '12 ingridients | 40 min',
-                style: TextStyle(
-                  color: AppColors.primary,
-                  fontSize: 16,
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
+          ),
+        )
+      ],
     );
   }
 }
