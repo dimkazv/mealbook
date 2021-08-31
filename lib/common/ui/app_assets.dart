a724e212d8b1b84d3fb8088aebf2d6f04d8f4b4b
Дмитрий Заречнев <zarechnevd@gmail.com>
2021-08-31 13:23:42 +0300

    Modified app assets

--- lib/common/ui/app_assets.dart
+++ lib/common/ui/app_assets.dart
@@ -1,7 +1,3 @@
 class AppAssets {
   static const splashPic = 'res/assets/splash.jpg';
-
-  static const homeIcon = 'res/assets/home.svg';
-  static const favoriteIcon = 'res/assets/favorite.svg';
-  static const shoppingListIcon = 'res/assets/shopping_list.svg';
 }
